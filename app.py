b65c061fc910e97ffb86c892459b47e9c2efff1a
Abdullah Al Mahfuz <almahfuz777@gmail.com>
2024-09-16 18:39:51 +0600

    create separate file for UI
    - detached UI code from llm chain
    - added sidebar
    - added dropdown menu to select among LLM models (currently llama3.1 and mistral)

--- app.py
+++ app.py
@@ -1,73 +1,36 @@
 import streamlit as st
-from langchain_community.embeddings import OllamaEmbeddings
-from langchain_chroma.vectorstores import Chroma
+from main import generate_ans
 from langchain_community.llms import Ollama
-from langchain.prompts import PromptTemplate
-from langchain.retrievers.multi_query import MultiQueryRetriever
-from langchain.prompts import ChatPromptTemplate
-from langchain_core.runnables import RunnablePassthrough
-from langchain_core.output_parsers import StrOutputParser
 import logging
-
-
-# Load the persisted ChromaDB vector store
-embedding = OllamaEmbeddings(model="nomic-embed-text", show_progress=True)
-persist_directory = "./db"
-
-vector_database = Chroma(
-    collection_name="local-rag",
-    persist_directory=persist_directory,
-    embedding_function=embedding
-)
-
-# Initialize the Ollama LLaMA 3.1 model
-llm = Ollama(model="llama3.1")
-
-
-QUERY_PROMPT = PromptTemplate(
-    input_variables=["question"],
-    template="""You are an AI language model specialized in physics. Your task is to reformulate the following question into five different versions to retrieve the most relevant physics documents.
-    Original question: {question}""",
-)
-
-retriever = MultiQueryRetriever.from_llm(
-    vector_database.as_retriever(search_kwargs={"k": 3}),
-    llm,
-    prompt=QUERY_PROMPT
-)
-
-# RAG prompt
-template = """You are a helpful assistant trained to answer physics questions based on the provided context.
-Use only the context below to answer the following question as clearly as possible:
-{context}
-Question: {question}
-"""
-
-prompt = ChatPromptTemplate.from_template(template)
-
-def post_process_answer(answer):
-    # Ensure the answer is focused on the physics topic
-    # Optionally trim any irrelevant parts or hallucinated information
-    processed_ans =  answer.split("Answer:")[-1].strip()
-    return processed_ans if processed_ans else "No relevant info found in the context..."
 
 # Streamlit UI
 st.title("Q/A bot Physics")
 
+# Sidebar for model selection and sessions
+with st.sidebar:
+    # dropdown menu to select a model
+    selected_model = st.selectbox(
+        "Select LLM",
+        options=["llama3.1", "mistral"],
+        index=0  # Default selection
+    )
+    
+    st.title("Chat Sessions") # Sidebar title
+
+# Setting up the LLM based on the selected model
+if selected_model == "llama3.1":
+    llm = Ollama(model="llama3.1")
+elif selected_model == "mistral":
+    llm = Ollama(model="mistral")
+
 user_ques = st.text_input("Throw your problems.")
+
 if st.button("Submit"):
     if user_ques:
         try:
             with st.spinner("Fetching AI response..."):
-                chain = (
-                    {"context": retriever, "question": RunnablePassthrough()}
-                    | prompt
-                    | llm
-                    | StrOutputParser()
-                )
-            answer = chain.invoke(user_ques)
-            final_ans = post_process_answer(answer)
-            st.write(f"Answer: {final_ans}")
+                final_ans = generate_ans(user_ques, llm)
+                st.write(f"Answer: {final_ans}")
         except Exception as e:
             st.error(f"An error occured: {str(e)}")
     else:
@@ -77,5 +40,6 @@ if st.button("Submit"):
 # logging after each response generation
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger()
+
 def log_query(user_question, answer):
     logger.info(f"Question: {user_question}, Answer: {answer}")
---
+++ main.py
@@ -0,0 +1,58 @@
+import streamlit as st
+from langchain_community.embeddings import OllamaEmbeddings
+from langchain_chroma.vectorstores import Chroma
+from langchain.prompts import PromptTemplate
+from langchain.retrievers.multi_query import MultiQueryRetriever
+from langchain.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.output_parsers import StrOutputParser
+
+# Load the persisted ChromaDB vector store
+embedding = OllamaEmbeddings(model="nomic-embed-text", show_progress=True)
+persist_directory = "./db"
+
+vector_database = Chroma(
+    collection_name="local-rag",
+    persist_directory=persist_directory,
+    embedding_function=embedding
+)
+
+QUERY_PROMPT = PromptTemplate(
+    input_variables=["context","question"],
+    template="""
+    You are an AI language model specialized in physics. Your task is to reformulate the following question into five different versions to retrieve the most relevant physics documents.
+    Original question: {question}""",
+)
+
+# RAG prompt
+template = """You are a helpful assistant trained to answer physics questions based on the provided context.
+Use only the context below to answer the following question as clearly as possible:
+{context}
+Question: 
+{question}
+"""
+
+prompt = ChatPromptTemplate.from_template(template)
+
+def post_process_answer(answer):
+    # Ensure the answer is focused on the physics topic
+    # Optionally trim any irrelevant parts or hallucinated information
+    processed_ans =  answer.split("Answer:")[-1].strip()
+    return processed_ans if processed_ans else "No relevant info found in the context..."
+
+def generate_ans(question, llm):
+    # Dynamic retriever that uses the passed LLM
+    retriever = MultiQueryRetriever.from_llm(
+        vector_database.as_retriever(search_kwargs={"k": 3}),
+        llm,
+        prompt = QUERY_PROMPT
+    )
+    chain = (
+        {"context": retriever, "question": RunnablePassthrough()}
+        | prompt
+        | llm
+        | StrOutputParser()
+    )
+    answer = chain.invoke(question)
+    final_ans = post_process_answer(answer)
+    return final_ans
